2902b3c2927091d8f08d3b8aa73cc459fdabdf2d
p555iii <690648264@qq.com>
2018-06-26 17:27:04 +0800

    Signed-off-by: p555iii <690648264@qq.com>

---
+++ service-zuul/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import com.example.demo.filter.MyFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public MyFilter myFilter(){
+        return new MyFilter();
+    }
+}
